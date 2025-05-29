e822a41a994225cc7caeb7da321c321e10002dcb
Kaikkonen Roope S <kaikkoro@cubbli24-gold.cs.helsinki.fi>
2025-05-29 19:17:30 +0300

    Add new item form

--- schema.sql
+++ schema.sql
@@ -4,3 +4,10 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    descriptio TEXT,
+    price INTEGER,
+    user_id INTEGER REFERENCES users
+);
